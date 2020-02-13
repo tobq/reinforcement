4f7af6147187ef696212474f293e5a7d70927ea8
tobq <tobi.akiny@gmail.com>
2020-02-13 16:44:16 +0000

    replaced close call with try with resources

--- src/main/java/tobi/reinforcement/Main.java
+++ src/main/java/tobi/reinforcement/Main.java
@@ -19,11 +19,11 @@ public class Main {
 //        private static final Problem PROBLEM = new Square();
 //    private static final Problem PROBLEM = new EqualsProblem(1);
     public static final double MUTATION_RATE = 0.1;
-    public static final int GENERATION_SIZE = 1000;
+    public static final int GENERATION_SIZE = 500;
     //    public static final int GENERATION_SIZE = 10000;
     private static final double PARENT_RATIO = 0.3;
     public static final int PARENT_COUNT = (int) Math.ceil(GENERATION_SIZE * PARENT_RATIO);
-    public static final long MAX_GENERATION_COUNT = 999999999;
+    public static final long MAX_GENERATION_COUNT = 250;
     private static final boolean ASEXUAL_REPRODUCTION = false;
     private static final boolean COPY_CROSSOVERS = true;
     private static final boolean CLONE_PARENTS = true;
@@ -61,8 +61,9 @@ public class Main {
      * - more generic problems, compare with other existing algorithms
      * - more complicated problems
      */
-    public static void main(String... args) throws InterruptedException, ExecutionException, IOException {
+    public static void main(String... args) throws InterruptedException, ExecutionException, IOException, URISyntaxException {
         Environment.init();
+//        final BoxDiscreteGym PROBLEM = new BoxDiscreteGym("MemorizeDigits-v0");
         final BoxBoxGym PROBLEM = new BoxBoxGym("BipedalWalker-v2");
 //        final ChartPanel chartPanel = new ChartPanel(ChartFactory.createXYLineChart(
 //                "",
@@ -87,77 +88,77 @@ public class Main {
                 "SORT_METHOD = " + SORT_METHOD,
                 "VARIABLE_ENVIRONMENT = " + VARIABLE_ENVIRONMENT
         };
-        PrintWriter fileWriter = new PrintWriter(new FileWriter(System.currentTimeMillis() + ".csv"));
+        try (PrintWriter fileWriter = new PrintWriter(new FileWriter(System.currentTimeMillis() + ".csv"))) {
 
-        for (String setting : CONFIG) {
-            System.out.println(setting);
-            fileWriter.println("#" + setting);
-        }
+            for (String setting : CONFIG) {
+                System.out.println(setting);
+                fileWriter.println("#" + setting);
+            }
 
 
-        int inputCount = PROBLEM.getInstanceLength();
-        int outputCount = PROBLEM.getLabelLength();
+            int inputCount = PROBLEM.getInstanceLength();
+            int outputCount = PROBLEM.getLabelLength();
 
-        Network[] generation = new Network[GENERATION_SIZE];
-        for (int i = 0; i < generation.length; i++) {
-            generation[i] = new Network(inputCount, outputCount);
-        }
+            Network[] generation = new Network[GENERATION_SIZE];
+            for (int i = 0; i < generation.length; i++) {
+                generation[i] = new Network(inputCount, outputCount);
+            }
 
-        ExecutorService exec = Executors.newCachedThreadPool();
-        int seed = random.nextPositiveInt();
-        for (int g = 0; g < MAX_GENERATION_COUNT; g++) {
-            double[] fitnesses = new double[generation.length];
+            ExecutorService exec = Executors.newCachedThreadPool();
+            int seed = random.nextPositiveInt();
+            for (int g = 0; g < MAX_GENERATION_COUNT; g++) {
+                double[] fitnesses = new double[generation.length];
 
-            ArrayList<Callable<Double>> tasks = new ArrayList<Callable<Double>>();
-            for (Network network : generation) {
-                int finalSeed = seed;
-                tasks.add(() -> PROBLEM.test(network, finalSeed));
+                ArrayList<Callable<Double>> tasks = new ArrayList<Callable<Double>>();
+                for (Network network : generation) {
+                    int finalSeed = seed;
+                    tasks.add(() -> PROBLEM.test(network, finalSeed));
 //                tasks.add(() -> PROBLEM.test(network));
-            }
-            List<Future<Double>> invokeAll = exec.invokeAll(tasks);
-            for (int i = 0; i < invokeAll.size(); i++) {
-                fitnesses[i] = invokeAll.get(i).get();
-            }
+                }
+                List<Future<Double>> invokeAll = exec.invokeAll(tasks);
+                for (int i = 0; i < invokeAll.size(); i++) {
+                    fitnesses[i] = invokeAll.get(i).get();
+                }
 //            for (int i = 0; i < GENERATION_SIZE; i++) {
 //                fitnesses[i] = PROBLEM.test(generation[i]);
 //            }
 
 
-            Integer[] order = new Integer[generation.length];
-            for (int i = 0; i < generation.length; i++) order[i] = i;
+                Integer[] order = new Integer[generation.length];
+                for (int i = 0; i < generation.length; i++) order[i] = i;
 
-            Network[] finalGeneration = generation;
+                Network[] finalGeneration = generation;
 
-            Comparator<Integer> c;
-            switch (SORT_METHOD) {
-                case LOG_SORT:
-                    c = Comparator
-                            .comparingDouble((Integer a) -> fitnesses[a] * Math.log(finalGeneration[a].getNeurons().size()))
-                            .reversed();
-                    break;
-                case TIE_MIN_NEURONS_SORT:
-                    c = Comparator
-                            .comparingDouble((Integer a) -> fitnesses[a])
-                            .reversed()
-                            .thenComparingInt(a -> finalGeneration[a].getNeurons().size());
-                    break;
-                default:
-                    c = Comparator
-                            .comparingDouble((Integer a) -> fitnesses[a])
-                            .reversed();
-                    break;
-            }
+                Comparator<Integer> c;
+                switch (SORT_METHOD) {
+                    case LOG_SORT:
+                        c = Comparator
+                                .comparingDouble((Integer a) -> fitnesses[a] * Math.log(finalGeneration[a].getNeurons().size()))
+                                .reversed();
+                        break;
+                    case TIE_MIN_NEURONS_SORT:
+                        c = Comparator
+                                .comparingDouble((Integer a) -> fitnesses[a])
+                                .reversed()
+                                .thenComparingInt(a -> finalGeneration[a].getNeurons().size());
+                        break;
+                    default:
+                        c = Comparator
+                                .comparingDouble((Integer a) -> fitnesses[a])
+                                .reversed();
+                        break;
+                }
 
-            Arrays.sort(order, c);
+                Arrays.sort(order, c);
 
-            int fittestIndex = order[0];
+                int fittestIndex = order[0];
 
-            double maxFitness = fitnesses[fittestIndex];
-            Network fittestNetwork = generation[fittestIndex];
-            int networkSize = fittestNetwork.getNeurons().size();
-            String networkString = fittestNetwork.serialise();
-            System.out.println(g + ": BEST FITNESS = " + maxFitness + "\t\tNode count = " + networkSize + "\t\t" + networkString);
-            fileWriter.println(maxFitness + "," + networkSize + "," + networkString);
+                double maxFitness = fitnesses[fittestIndex];
+                Network fittestNetwork = generation[fittestIndex];
+                int networkSize = fittestNetwork.getNeurons().size();
+                String networkString = fittestNetwork.serialise();
+                System.out.println(g + ": BEST FITNESS = " + maxFitness + "\t\tNode count = " + networkSize + "\t\t" + networkString);
+                fileWriter.println(maxFitness + "," + networkSize + "," + networkString);
 //            System.out.println("WORST FITNESS = " + fitnesses[indices[GENERATION_SIZE - 1]]);
 //            System.out.println("AVG FITNESS = " + Arrays.stream(fitnesses).average().getAsDouble());
 
@@ -166,28 +167,28 @@ public class Main {
 //            for (int i = 0; i < GENERATION_SIZE; i++) System.out.print(fitnesses[order[i]] + ", ");
 //            System.out.println();
 //
-            final Network[] nextGen = new Network[generation.length];
-            if (CLONE_PARENTS) {
-                for (int i = 0; i < PARENT_COUNT; i++)
-                    nextGen[i] = generation[order[i]].clone();
-            } else {
-                for (int i = 0; i < PARENT_COUNT; i++)
-                    nextGen[i] = generation[order[i]].copy();
-            }
-            for (int i = PARENT_COUNT; i < generation.length; i++) {
-                if (ASEXUAL_REPRODUCTION) {
-                    nextGen[i] = generation[order[i]].copy();
+                final Network[] nextGen = new Network[generation.length];
+                if (CLONE_PARENTS) {
+                    for (int i = 0; i < PARENT_COUNT; i++)
+                        nextGen[i] = generation[order[i]].clone();
                 } else {
-                    int randomIndexA = Utils.logRandom(generation);
-                    int randomIndexB = Utils.logRandom(generation);
-                    Network a = generation[randomIndexA];
-                    Network b = generation[randomIndexB];
-                    Network crossover = Network.crossover(a, b, fitnesses[randomIndexA], fitnesses[randomIndexB]);
-                    if (COPY_CROSSOVERS) crossover = crossover.copy();
-                    nextGen[i] = crossover;
+                    for (int i = 0; i < PARENT_COUNT; i++)
+                        nextGen[i] = generation[order[i]].copy();
                 }
-            }
-            generation = nextGen;
+                for (int i = PARENT_COUNT; i < generation.length; i++) {
+                    if (ASEXUAL_REPRODUCTION) {
+                        nextGen[i] = generation[order[i]].copy();
+                    } else {
+                        int randomIndexA = Utils.logRandom(generation);
+                        int randomIndexB = Utils.logRandom(generation);
+                        Network a = generation[randomIndexA];
+                        Network b = generation[randomIndexB];
+                        Network crossover = Network.crossover(a, b, fitnesses[randomIndexA], fitnesses[randomIndexB]);
+                        if (COPY_CROSSOVERS) crossover = crossover.copy();
+                        nextGen[i] = crossover;
+                    }
+                }
+                generation = nextGen;
 
 
 //            SwingUtilities.invokeLater(() -> showGraph(nextGen[0]));
@@ -195,9 +196,9 @@ public class Main {
 //            plotChart(chartPanel, fittestNetwork, GOAL_FUNC);
 //            jFrame.pack();
 //            jFrame.setVisible(true);
-            if (VARIABLE_ENVIRONMENT) seed = random.nextPositiveInt();
+                if (VARIABLE_ENVIRONMENT) seed = random.nextPositiveInt();
+            }
         }
-        fileWriter.close();
     }
 
     private static class MyRandom extends Random {
